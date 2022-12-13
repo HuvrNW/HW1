75b90dcb5209a6d954ceb2c25e5ea18ff44dc571
С - 3 < - 3@DESKTOP-E0FTS21>
2022-12-13 18:50:26 +0300

    Добавьте файлы проекта.

---
+++ HW1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW1
+{
+    internal class Program
+    {
+        static void PrintName(string _name)
+        {
+            Console.WriteLine("Hello, ");
+                for (int i = 0; i < _name.Length; i += 2)
+                {
+                    Console.Write(_name[i] + "\n");
+                }
+            Console.WriteLine("!");
+        }
+
+        static void Main(string[] args)
+        {
+            string name = "";
+           
+                Console.WriteLine("Как вас зовут?");
+                name = Console.ReadLine();
+                PrintName(name);
+
+            Console.ReadKey();
+        }
+    }
+}
